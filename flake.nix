3aea64b4def1b42cc451982309aa8b4df2bbda39
John Shaffer <john@insilica.code>
2023-05-04 12:12:40 -0500

    Add codescene, joyride, remote-containers, and direnv extensions

--- flake.nix
+++ flake.nix
@@ -9,19 +9,45 @@
     flake-utils.lib.eachDefaultSystem (system:
       with import nixpkgs { inherit system; };
       let
-        extensions = (with vscode-extensions; [
-          bbenoist.nix
-          betterthantomorrow.calva
-          brettm12345.nixfmt-vscode
-          codezombiech.gitignore
-          editorconfig.editorconfig
-          graphql.vscode-graphql
-          kahole.magit
-          ms-vscode-remote.remote-ssh
-          ms-vsliveshare.vsliveshare
-          rust-lang.rust-analyzer
-          timonwong.shellcheck
-        ]);
+        extensions = (with vscode-extensions;
+          [
+            bbenoist.nix
+            betterthantomorrow.calva
+            brettm12345.nixfmt-vscode
+            codezombiech.gitignore
+            editorconfig.editorconfig
+            graphql.vscode-graphql
+            kahole.magit
+            ms-vscode-remote.remote-ssh
+            ms-vsliveshare.vsliveshare
+            rust-lang.rust-analyzer
+            timonwong.shellcheck
+          ] ++ (pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "codescene-vscode";
+              publisher = "CodeScene";
+              version = "0.2.1";
+              sha256 = "sha256-EslE2ee9EoPa3bOGNAxtTdj5QRs9JuaF/efYu9rHOCw=";
+            }
+            {
+              name = "joyride";
+              publisher = "betterthantomorrow";
+              version = "0.0.33";
+              sha256 = "sha256-bHQKCdq1nQI1GFcX4AgSULDH5kOk5DARnWZoUga8u30=";
+            }
+            {
+              name = "remote-containers";
+              publisher = "ms-vscode-remote";
+              version = "0.293.0";
+              sha256 = "sha256-O0hM+GKZ46moihBlPvfNuqfLazR5WVqEaE6XiEnaNN0=";
+            }
+            {
+              name = "vscode-direnv";
+              publisher = "cab404";
+              version = "1.0.0";
+              sha256 = "0xikkhbzb5cd0a96smj5mr1sz5zxrmryhw56m0139sbg7zwwfwps";
+            }
+          ]));
         vscode = pkgs.vscode-with-extensions.override {
           vscodeExtensions = extensions;
         };
